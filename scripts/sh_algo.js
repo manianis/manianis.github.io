90c3c7df4702b38cc10d476a4d72727afe83f1e3
Mohamed Anis MANI <manianis@yahoo.fr>
2018-10-09 09:14:04 +0100

    Chapitre 4 et 5 presque terminés

--- scripts/sh_algo.js
+++ scripts/sh_algo.js
@@ -4,7 +4,7 @@ if (! this.sh_languages) {
 sh_languages['algo'] = [
   [
     [
-      /\b(?:alfa|et|tableau|début|selon|const|div|faire|downto|sinon|fin|faux|fichier|pour|fonction|get|goto|si|dans|label|mod|new|non|de|ou|pack|packed|page|programme|put|procedure|lire|lirenl|enregistrement|répéter|repeter|ouvrir|recreer|ensemble|texte|alors|à|vrai|type|unpack|jusqu|var|tantque|avec|ecrirenl|ecrire)\b/gi,
+      /\b(?:alfa|et|tableau|début|selon|const|div|faire|downto|sinon|fin|faux|fichier|pour|def|fn|get|goto|si|dans|label|mod|new|non|de|ou|pack|packed|page|programme|put|proc|lire|lirenl|enregistrement|répéter|repeter|ouvrir|recreer|ensemble|texte|alors|à|vrai|type|unpack|jusqu|var|tantque|avec|ecrirenl|ecrire)\b/gi,
       'sh_keyword',
       -1
     ],
@@ -34,7 +34,7 @@ sh_languages['algo'] = [
     //   4
     // ],
     [
-      /\b(?:booleen|octet|caractere|entier|maxint|reel|chaine|entier_long|extended|mot|double|simple)\b/gi,
+      /\b(?:booléen|booleen|octet|caractere|caractère|entier|maxint|reel|chaine|entier_long|extended|mot|double|simple)\b/gi,
       'sh_type',
       -1
     ],
